439de6b9c5fe986e5316ba45cfd491ef10b457bd
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 21:39:02 +0300

    Add files via upload

---
+++ main (8).php
@@ -0,0 +1,13 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// Sabit Değişkenli Diziler
+define("ad",array("Emre","mehmet","ali","veli"));
+echo(ad[0]);
+define("takim",["gala","beşik","fener"]);
+echo(takim[0]);
+
+?>
